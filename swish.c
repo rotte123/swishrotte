35b9e525b70300b6bce539d19ab45e9c2c9af38f
rotte123 <53999164+rotte123@users.noreply.github.com>
2019-08-11 21:00:37 +0530

    Add files via upload

---
+++ swish.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#include <conio.h>
+
+#include <math.h>
+
+
+int main()
+
+{
+
+	float mrp,disc,totalcost=0.0;
+
+    	scanf("%f %f",&mrp,&disc);
+
+	printf("\n");
+
+	while(mrp>=1.0)
+
+	{
+
+		totalcost+=mrp;
+
+		mrp=truncf(mrp-(mrp*(disc/100)));
+
+	}
+
+	printf("%.2f",totalcost);
+	return 0;
+
+}
